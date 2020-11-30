6feedf23e12323600f9158e274857742d51012e1
HaneulSky <tanya0610@naver.com>
2020-11-30 20:04:14 +0300

    подготовлены файлы для работы с JS

---
+++ src/scripts/components/NewsCardList.js
@@ -0,0 +1,9 @@
+class NewsCardList {
+  constructor (container, api, createCardFunction){
+    this.container = container;
+    this.api = api;
+    this.createCardFunction = createCardFunction;
+  }
+
+
+}
